551c7cc76f2ac5eb909c9ddae4bb4e2e8b81513a
sarin-the-clone <71705278+sarin-the-clone@users.noreply.github.com>
2021-03-08 16:18:53 +0100

    Update gogo.py

--- gogo.py
+++ gogo.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import lol
 
 df_temp_all = pd.read_pickle('dftempall.pickle')
+
 df_temp_all.head()
+df_temp_all['sourceId'].unique()
+
+s = df_temp_all['sourceID'].sample().values[0]
+
+df_temp_s = df_temp_all[df_temp_all['sourceID'] == s]
+
 
