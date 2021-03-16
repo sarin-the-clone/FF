f21aaad12b291b009d38a414bf62affdeb58bceb
sarin-the-clone <71705278+sarin-the-clone@users.noreply.github.com>
2021-03-16 14:51:40 +0100

    Update gogo.py

--- gogo.py
+++ gogo.py
@@ -40,45 +40,80 @@ if disp: df_aGDD_s.plot()
 # n_years_2train = #years for the training set
 dhist, dhoriz, n_years_2train = int(365/2), int(365/4), 3
 
+
+##############################################################################################################
+##### using lstm #####
+##############################################################################################################
+
 # generates lstm model for s (takes about 1-2 minutes) :
 src = lol.weather_station(df_t, dhist, dhoriz, n_years_2train, 'lstm')
 
-
-##### plot input temperatures and forecast #####
 start = src.data.index[src.year_train*365+src.dd_historic+src.dd_horizon] #first day after validation data
 end =  df_t.index[-src.dd_horizon-1]
+Harvest, Harvest_hat = start, start
 for today in pd.date_range(start,end):
-    plt.cla()
     ind_curr = pd.date_range(today-timedelta(dhist-1),today)
     x_input = df_t.loc[ind_curr]
     yhat = yhat = src.lstm_predict(x_input)
 
+    plt.figure(1), plt.cla()
     x_input.plot(style='b-')
     df_t.loc[yhat.index].plot(style='g-')
     yhat.plot(style='r--')
     plt.pause(0.0001)
 
-
-##### plot accumulated temperatures and forecast #####
-Harvest, Harvest_hat = start, start
-for today in pd.date_range(start,end):
-    plt.cla()
-    ind_curr = pd.date_range(today-timedelta(dhist-1),today)
-    x_input = df_t.loc[ind_curr]
-    yhat = yhat = src.lstm_predict(x_input)
     ind_pred = yhat.index
     yhat = lol.gdd(df_t.loc[pd.date_range(today-timedelta(365),today)].append(yhat))
 
     ind_curr_year = df_aGDD_s[df_aGDD_s.index. year == today.year].index
     Harvest = df_aGDD_s[ind_curr_year][df_aGDD_s[ind_curr_year] <= 500].index[-1].date()
+
     ind_curr_year = yhat[yhat.index. year == today.year].index
     Harvest_hat = yhat[ind_curr_year][yhat[ind_curr_year] <= 500].index[-1].date()
 
-    plt.title(['today: '+str(today.date()), 'HT: ' + str(Harvest), 'HT_hat: ' + str(Harvest_hat), 
-               '(today - HT,HT_hat - HT): (' + str((Harvest - today.date()).days) + ',' + str((Harvest_hat - Harvest).days) + ')' ])
+    plt.figure(2), plt.cla()
     df_aGDD_s[ind_curr].plot(style='b-')
     df_aGDD_s.loc[ind_pred].plot(style='g-')
     yhat[ind_pred].plot(style='r--')
-    plt.legend('past temperatures', 'future temperatures', 'forecast')
+    plt.legend(['past temperatures', 'future temperatures', 'forecast'])
+    plt.title(('today: '+str(today.date()), 'HT: ' + str(Harvest), 'HT_hat: ' + str(Harvest_hat),
+               '(today - HT,HT_hat - HT): (' + str((Harvest - today.date()).days) + ',' + str((Harvest_hat - Harvest).days) + ')' ))
+
+    plt.pause(0.0001)
+    
+##############################################################################################################    
+###### using Prophet #####
+##############################################################################################################
+start = df_t.index[int(src.year_train*365)] 
+end =  df_t.index[-src.dd_horizon-1]
+for today in pd.date_range(start,end,freq='2W'):
+    ind_train = pd.date_range(df_t.index[0],today)
+    ind_curr = pd.date_range(today-timedelta(dhist-1),today)
+    ind_pred = pd.date_range(today+timedelta(1),today+timedelta(dhoriz))
+    
+    # generates prophet model:
+    src = lol.weather_station(df_t.loc[ind_train], dhist, dhoriz, n_years_2train, 'prophet')
+    yhat = src.ph_predict()
+    yhat.loc[ind_train] = df_t.loc[ind_train].copy()
+
+    plt.figure(1),plt.cla()
+    df_t.loc[ind_curr].plot(style='b-')
+    df_t.loc[ind_pred].plot(style='g-')
+    yhat[ind_pred].plot(style='r--')
+
+    yhat = lol.gdd(yhat)
+    ind_curr_year = df_aGDD_s[df_aGDD_s.index. year == today.year].index
+    Harvest = df_aGDD_s[ind_curr_year][df_aGDD_s[ind_curr_year] <= 500].index[-1].date()
+
+    ind_curr_year = yhat[yhat.index. year == today.year].index
+    Harvest_hat = yhat[ind_curr_year][yhat[ind_curr_year] <= 500].index[-1].date()
+    
+    plt.figure(2),plt.cla()
+    df_aGDD_s.loc[ind_curr].plot(style='b-')
+    df_aGDD_s.loc[ind_pred].plot(style='g-')
+    yhat[ind_pred].plot(style='r--')
+    plt.title(('today: '+str(today.date()), 'HT: ' + str(Harvest), 'HT_hat: ' + str(Harvest_hat),
+                '(today - HT,HT_hat - HT): (' + str((Harvest - today.date()).days) + ',' + str((Harvest_hat - Harvest).days) + ')' ))
+
     plt.pause(0.0001)
 
